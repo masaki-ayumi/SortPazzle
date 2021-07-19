ac264b0d6f15d2d8ca6b993843247381c6801168
DESKTOP-ROER1NG\まっちゃん <ilovegames.ilikesplatoon@gmail.com>
2021-07-19 17:23:38 +0900

    パズルに必要なオブジェクトをスクリプトで画面に表示

---
+++ Assets/Scripts/BallCreate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallCreate : MonoBehaviour
+{
+    public GameObject prefabObject;
+    public GameObject objectPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateGameobject();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CreateGameobject()
+    {
+       Transform objPosition = objectPosition.transform;
+       GameObject obj = Instantiate(prefabObject, objPosition.position, Quaternion.identity);
+    }
+}
