a4630ce42fdf7ece4e02d345f82ddba553606c19
DESKTOP-ROER1NG\まっちゃん <ilovegames.ilikesplatoon@gmail.com>
2021-07-31 01:12:27 +0900

    オブジェクトを触ったあとの処理を作るために触ったかを判定するフラグを追加

--- Assets/Scripts/TapController.cs
+++ Assets/Scripts/TapController.cs
@@ -4,29 +4,35 @@ using UnityEngine;
 
 public class TapController : MonoBehaviour
 {
+    private bool isTouch;        //オブジェクトを触ったか判定するフラグ
     // Start is called before the first frame update
     void Start()
     {
-        
+        isTouch = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //関数が動いたら別の関数を動かす
+        if (isTouch == true)
+        {
+            Debug.Log("別の関数");
+        }
+        isTouch = false;
     }
 
     public void TapObject()
     {
-        //Debug.Log("触った");
+        isTouch = true;
         //スクリプトをアタッチしたオブジェクトを親オブジェクトとする
         GameObject parent = this.gameObject;
 
         //親オブジェクトの一つ下の子オブジェクトを取得
         GameObject child = parent.transform.GetChild(0).gameObject;
 
-        Debug.Log(child);        
-
+        //Debug.Log(child);        
+        //子オブジェクトから孫オブジェクトを取得
         GameObject mago = child.transform.GetChild(0).gameObject;
         Debug.Log(mago);
     }
