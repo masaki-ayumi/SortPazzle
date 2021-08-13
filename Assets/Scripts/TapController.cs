d06820b73cd0c4057a8a4c277bb79944616ef72e
DESKTOP-ROER1NG\まっちゃん <ilovegames.ilikesplatoon@gmail.com>
2021-08-14 01:52:11 +0900

    スクリプトを全てKUSI(空のオブジェクト)にアタッチ 回数ごとに違う関数を実行することに成功 奇数と偶数で実行する関数を切り替えた

--- Assets/Scripts/TapController.cs
+++ Assets/Scripts/TapController.cs
@@ -8,16 +8,24 @@ public class TapController : MonoBehaviour
 
     private GameObject tempObject;
 
+    public float count;
 
     // Start is called before the first frame update
     void Start()
     {
         isTouch = false;
+        count = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Debug.Log(count);
+
+        if(count>=3)
+        {
+            count = 0;
+        }
         //関数が動いたら別の関数を動かす
         //if (isTouch == true)
         //{
@@ -29,7 +37,14 @@ public class TapController : MonoBehaviour
 
     public void TapObject()
     {
-        isTouch = true;
+        count += 1;
+
+        if (count!=1)
+        {
+            return;
+        }
+        //TODO：カウント変数を用意して奇数と偶数で関数を切り替える
+        //isTouch = true;
 
         GameObject parent = null;
 
@@ -61,8 +76,48 @@ public class TapController : MonoBehaviour
 
         tempObject = mago;
 
-        //メモ　違うスクリプトを作り別のタップ関数を作成してタップを判定するフラグを渡してタップした時だけ動くようにしてみよう
 
+    }
+
+    public void Move()
+    {
+        if (count!=2)
+        {
+            return;
+        }
+        count += 1;
+
+        //isTouch = false;
+
+
+        GameObject parent = null;
+
+        GameObject tapGameobject;
+        //マウスでタッチしたオブジェクトを親オブジェクトとする
+        if (Input.GetMouseButtonDown(0))
+        {
+            tapGameobject = null;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit2D = Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction);
+
+            if (hit2D)
+            {
+                tapGameobject = hit2D.transform.gameObject;
+                parent = tapGameobject;
+            }
+        }
+
+        //スクリプトをアタッチしたオブジェクトを親オブジェクトとする
+        //GameObject parent = this.gameObject;
+
+        //親オブジェクトの一つ下の子オブジェクトを取得
+        GameObject child = parent.transform.GetChild(0).gameObject;
+
+        //Debug.Log(child);        
+        //子オブジェクトから孫オブジェクトを取得
+        GameObject mago = child.transform.GetChild(0).gameObject;
+        Debug.Log(mago);
+        Debug.Log("一回目に選んだ団子とここで選んだ団子の座標を入れ替える");
     }
 
 
