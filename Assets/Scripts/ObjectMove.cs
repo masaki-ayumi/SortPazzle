3db5217f731a86c188f596cdf2c1a0f08a249d1a
DESKTOP-ROER1NG\まっちゃん <ilovegames.ilikesplatoon@gmail.com>
2021-08-14 01:32:57 +0900

    これはいらないかもしれない

--- Assets/Scripts/ObjectMove.cs
+++ Assets/Scripts/ObjectMove.cs
@@ -4,30 +4,25 @@ using UnityEngine;
 
 public class ObjectMove : MonoBehaviour
 {
-    public bool isTouch;
+    public bool isTouch;        //二回目のタッチを判断するためのフラグ
+    TapController tapScript;    //一回目のタッチを受け取るためのスクリプト変数
     // Start is called before the first frame update
     void Start()
     {
         isTouch = false;
+        tapScript = this.gameObject.GetComponent<TapController>();
+        //isTouch = tapScript.isTouch;
     }
 
     // Update is called once per frame
     void Update()
     {
-        TapController tapScript = this.gameObject.GetComponent<TapController>();
+        //一回目のisTouchの状態を受け取る
         isTouch = tapScript.isTouch;
+
         if (isTouch == false)
             tapScript.isTouch = isTouch;
     }
 
-    public void Move()
-    {
-        if (isTouch==false)
-        {
-            return;
-        }
-
-        Debug.Log("団子タッチ");
-        isTouch = false;
-    }
+    
 }
