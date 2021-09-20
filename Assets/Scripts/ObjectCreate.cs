a66df090cc59a1b83f14d5d93aad2da4e1f212b3
MatTyaN0125 <ilovegames.ilikesplatoon@gmail.com>
2021-09-20 18:05:43 +0900

    団子オブジェクトの生成の仕方を変更

--- Assets/Scripts/ObjectCreate.cs
+++ Assets/Scripts/ObjectCreate.cs
@@ -7,6 +7,10 @@ public class ObjectCreate : MonoBehaviour
     public GameObject kusiPrefab;       //串Prefabをアタッチ
     public GameObject[] dangoPrefab;    //それぞれの団子のPrefabをアタッチ
     public GameObject[] dangoPosition;  //団子の座標になる空オブジェクトをアタッチ
+    public GameObject[] KUSIPosition;  //串の座標になる空オブジェクトをアタッチ
+
+    int count = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,18 +28,25 @@ public class ObjectCreate : MonoBehaviour
     //串のオブジェクト生成用関数
     public void CreateKUSIObject()
     {
-        //スクリプトをアタッチしたオブジェクトを親オブジェクトにする
-        Transform parent = this.transform;
+        //親オブジェクト配列
+        Transform[] parent = new Transform[3];
 
-        //串Prefabをインスタンス化
-        GameObject kusiObject = Instantiate(kusiPrefab, this.transform.position, Quaternion.identity, parent);
+        for (int i = 0; i < KUSIPosition.Length; i++)
+        {
+            //串用の座標オブジェクトを親オブジェクトにする
+            parent[i] = KUSIPosition[i].transform;
+
+
+            //串Prefabをインスタンス化
+            GameObject kusiObject = Instantiate(kusiPrefab, parent[i].transform.position, Quaternion.identity, parent[i]);
+        }
     }
 
     //お団子のオブジェクト生成用関数
     public void CreateDANGOObject()
     {
         //親オブジェクト配列
-        Transform[] parent = new Transform[4];
+        Transform[] parent = new Transform[12];
 
         for (int i = 0; i < dangoPosition.Length; i++)
         {
@@ -43,23 +54,26 @@ public class ObjectCreate : MonoBehaviour
             parent[i] = dangoPosition[i].transform;
 
             //団子Prefabをインスタンス化
-            GameObject dangoObject = Instantiate(dangoPrefab[Random.Range(0, 3)], parent[i].transform.position, Quaternion.identity, parent[i]);
+            GameObject dangoObject = Instantiate(dangoPrefab[Random.Range(0, 7)], parent[i].transform.position, Quaternion.identity, parent[i]);
 
 
             /*TODO:生成される団子prefabを制限する
                    一種類につき四つまでに制限*/
 
-            //dangoPrefab[]とdangoObjectをつかって生成された団子をカウントする
-            //switch文を使う？
+            //tagをつかって生成された団子をカウントする
 
-            if (dangoObject == dangoPrefab[1])
-                Debug.Log("aaaaa"); //なぜか通らない
+            if (dangoObject.tag == "GOMA")
+            {
+                count++;
+
+            }
 
 
             GameObject temp = dangoObject;
-            Debug.Log(dangoPrefab[1]);
+            Debug.Log(dangoObject);
+            Debug.Log(count);
         }
     }
 
-    
+
 }
