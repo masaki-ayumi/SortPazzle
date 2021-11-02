8191dd8ccb24331d2115d154e31c79f72360b8e3
DESKTOP-ROER1NG\まっちゃん <ilovegames.ilikesplatoon@gmail.com>
2021-11-03 01:58:09 +0900

    効果音を追加する

---
+++ Assets/Scripts/AudioScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// BGMや効果音を制御するスクリプト
+/// </summary>
+public class AudioScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
